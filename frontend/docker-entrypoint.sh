2bebb956d1274dfdfd3cb0a5187f2298848bdc62
Alex Sieland <github@nightgecko.me>
2026-03-02 18:16:55 -0500

    Populate config in dockerfile

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,5 +3,8 @@
 # Override the nginx configuration with environment variables
 envsubst '$BACKEND_PORT $EXPOSE_SWAGGER_UI $NGINX_HOST' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
+# Inject API_URL into config.js
+envsubst '$API_URL' < /usr/share/nginx/html/config.js.template > /usr/share/nginx/html/config.js
+
 # Start nginx in the foreground
 nginx -g 'daemon off;'
